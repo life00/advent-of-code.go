0569513190ac3aee0d50b4642354e081dc92ce9f
life00 <contact.5lu1v@simplelogin.co>
2024-07-25 14:19:26 +0200

    2023 day 1: split code into functions and improve logging

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -10,6 +10,7 @@ import (
 
 // function that fixes word numbers that overlap e.g. (tw(o)ne)
 func fixInput(rawInput *[]byte) {
+	fmt.Printf("Fixing broken word numbers...\n")
 	// temporarily save it as other variable
 	input := string(*rawInput)
 	// all broken word number combinations
@@ -17,16 +18,19 @@ func fixInput(rawInput *[]byte) {
 	strings := [8]string{"oneeight", "twoone", "threeeight", "fiveeight", "sevennine", "eighttwo", "eightthree", "nineeight"}
 	// loop through arrays
 	for i := 0; i < 8; i++ {
+		fmt.Printf("%s --> %s\n", brokenStrings[i], strings[i])
 		// replace strings[i] with ints[i]
 		reg := regexp.MustCompile(brokenStrings[i])
 		input = reg.ReplaceAllString(input, strings[i])
 	}
+	fmt.Printf("Done!\n")
 	// assign it back
 	*rawInput = []byte(input)
 }
 
 // function that replaces proper word numbers with numbers
 func replaceNumbers(rawInput *[]byte) {
+	fmt.Printf("Replacing word numbers with numbers...\n")
 	// define strings and ints
 	strings := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 	ints := [9]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
@@ -34,12 +38,58 @@ func replaceNumbers(rawInput *[]byte) {
 	input := string(*rawInput)
 	// loop through arrays
 	for i := 0; i < 9; i++ {
+		fmt.Printf("%s --> %s\n", strings[i], ints[i])
 		// replace strings[i] with ints[i]
 		reg := regexp.MustCompile(strings[i])
 		input = reg.ReplaceAllString(input, ints[i])
 	}
+	fmt.Printf("Done!\n")
 	// assign it back
 	*rawInput = []byte(input)
+}
+
+// extracts the numbers to an array
+func getNumbers(rawInput *[]byte) (numbers []int) {
+	fmt.Printf("Getting numbers from the data...\n")
+	// split it into an array based on newline character
+	arrInput := strings.Split(string(*rawInput), "\n")
+	// remove the last faulty element in the array
+	if len(arrInput) > 0 {
+		arrInput = arrInput[:len(arrInput)-1]
+	}
+	// preparing environment
+	reg, err := regexp.Compile("[^0-9]+") // regex for removing non digits
+	if err != nil {
+		fmt.Println(err)
+	}
+	// loop through every line
+	for _, s := range arrInput {
+		fmt.Printf("%s --> ", s)
+		// remove all non digit characters
+		s = reg.ReplaceAllString(s, "")
+		// get the first and last character of a string
+		s = string(s[0]) + string(s[len(s)-1])
+		// convert string to integer
+		d, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%d\n", d)
+		// append the digit to an array
+		numbers = append(numbers, d)
+	}
+	fmt.Printf("Done!\n")
+	return numbers
+}
+
+// sums all the numbers in the array
+func sumNumbers(numbers *[]int) (result int) {
+	fmt.Printf("Summing the array...\n%d\n", numbers)
+	for _, d := range *numbers {
+		result += d
+	}
+	fmt.Printf("Done!\n")
+	return result
 }
 
 func main() {
@@ -50,32 +100,8 @@ func main() {
 	}
 	fixInput(&rawInput)
 	replaceNumbers(&rawInput)
-	// split it into an array based on newline character
-	arrInput := strings.Split(string(rawInput), "\n")
-	// remove the last faulty element in the array
-	if len(arrInput) > 0 {
-		arrInput = arrInput[:len(arrInput)-1]
-	}
-	// preparing environment
-	reg, err := regexp.Compile("[^0-9]+") // regex for removing non digits
-	result := 0                           // saving all the digits
-	if err != nil {
-		fmt.Println(err)
-	}
-	// loop through every line
-	for _, s := range arrInput {
-		// remove all non digit characters
-		s = reg.ReplaceAllString(s, "")
-		// get the first and last character of a string
-		s = string(s[0]) + string(s[len(s)-1])
-		// convert string to integer
-		d, err := strconv.Atoi(s)
-		if err != nil {
-			fmt.Println(err)
-		}
-		// add the value to the sum
-		result += d
-	}
+	numbers := getNumbers(&rawInput)
+	result := sumNumbers(&numbers)
 	// print out the result
-	fmt.Printf("%d\n", result)
+	fmt.Printf("Here is the result: %d\n", result)
 }
