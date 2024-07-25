372eae209e969cc954ef42eb30a619c33a1c0b5c
life00 <contact.5lu1v@simplelogin.co>
2024-07-25 13:52:29 +0200

    2023 day 2: add constants and getGameID

--- 2023/day2/day2.go
+++ 2023/day2/day2.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func checkGame(raw *string) int {
-	fmt.Printf("Checking: %s\n", *raw)
+const (
+	maxRed   = 12
+	maxGreen = 13
+	maxBlue  = 14
+)
+
+// checks if game is possible
+// returns game ID if possible, or 0 if not
+func checkGame(game string) int {
+	id := getGameID(game)
+	fmt.Printf("Checking game %d...\n", id)
 	// ...
 	return 0
 }
 
+// gets the game ID
+func getGameID(game string) (id int) {
+	// remove the prefix and suffix of the game data
+	game, _ = strings.CutPrefix(game, "Game ")
+	game, _, _ = strings.Cut(game, ":")
+	// convert to integer
+	id, err := strconv.Atoi(game)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return id
+}
+
 func main() {
+	// print out the maximum values
+	fmt.Printf("Maximum number of cubes for the games: %d red, %d green, and %d blue.\n", maxRed, maxGreen, maxBlue)
 	// read the input file
 	bytes, err := os.ReadFile("./input.txt")
 	if err != nil {
@@ -28,9 +53,13 @@ func main() {
 	// variable to sum all possible games
 	var sum int
 
+	// loop through all the games
 	for _, v := range games {
-		sum += checkGame(&v)
+		// add the game ID
+		// if the game is impossible it will return 0
+		sum += checkGame(v)
 	}
 
-	fmt.Println(sum)
+	// print the result
+	fmt.Printf("Sum of all possible game IDs: %d\n", sum)
 }
